ec9c1f00f5d9d4b4e55851c7fe2ecfafa7c1a9f6
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2026-02-18 16:13:04 +0700

    s

--- mini_project01/bin/mp_perpus/main.dart
+++ mini_project01/bin/mp_perpus/main.dart
@@ -1,12 +1,5 @@
 import 'bukuperpus.dart';
 
 void main() {
-  var buku = BukuPerpus("Matematika", 4)
-    ..showInfo()
-    ..pinjam()
-    ..pinjam()
-    ..pinjam()
-    ..pinjam()
-    ..pinjam()
-    ..kembali();
+  
 }
